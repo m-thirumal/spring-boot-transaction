f02fc5adfd59c8e95b280052237c95e4be6a5898
Thirumal <m.thirumal@hotmail.com>
2024-09-21 12:04:42 +0530

    first commit

---
+++ src/test/java/com/thirumal/spring_boot_transaction/SpringBootTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirumal.spring_boot_transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
